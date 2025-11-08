8df58a1e6472295d61bef7b8f2e9d70efe4ee223
Carlos Vicente Tontaquimba Quinchuqui <Carlos.Tontaquimba@fj.local>
2025-11-08 09:52:57 -0500

    fix: Trabajando en generacion de encuentros con equipo local y visitante

--- src/main/java/pawkar/backend/entity/Encuentro.java
+++ src/main/java/pawkar/backend/entity/Encuentro.java
@@ -16,12 +16,24 @@ import java.time.LocalDateTime;
 public class Encuentro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "encuentro_id", columnDefinition = "SERIAL")
+    @Column(name = "encuentro_id", columnDefinition = "BIGSERIAL")
     private Integer id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "subcategoria_id", nullable = false)
     private Subcategoria subcategoria;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "estadio_id", nullable = false)
+    private Estadio estadio;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "equipo_local_id", nullable = false)
+    private Equipo equipoLocal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "equipo_visitante_id", nullable = false)
+    private Equipo equipoVisitante;
 
     @Column(nullable = false, length = 150)
     private String titulo;
@@ -29,13 +41,9 @@ public class Encuentro {
     @Column(name = "fecha_hora", nullable = false)
     private LocalDateTime fechaHora;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "estadio_id", nullable = false)
-    private Estadio estadio;
-
     @Column(length = 50)
     private String estado = "Pendiente";
 
-    @Column(columnDefinition = "BOOLEAN DEFAULT TRUE")
+    @Column(nullable = false)
     private Boolean activo = true;
 }
