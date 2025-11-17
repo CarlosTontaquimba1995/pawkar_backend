f0ec4879b9bc8f0e413d3a00ee76fa9b145a756e
Carlos Vicente Tontaquimba Quinchuqui <Carlos.Tontaquimba@fj.local>
2025-11-17 09:23:32 -0500

    fix: csript base de datos actualizado

--- db.sql
+++ db.sql
@@ -18,7 +18,9 @@ CREATE TABLE IF NOT EXISTS roles (
      id BIGSERIAL PRIMARY KEY,
      name VARCHAR(50) NOT NULL UNIQUE,
      detail VARCHAR(100) NOT NULL unique,
-     estado BOOLEAN default true, 
+     estado BOOLEAN default true,
+     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
  );
 
 -- Tabla de Unión Usuario-Roles
@@ -26,6 +28,8 @@ CREATE TABLE IF NOT EXISTS user_roles (
      user_id BIGINT NOT NULL,
      role_id BIGINT NOT NULL,
      PRIMARY KEY (user_id, role_id),
+     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
      FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
@@ -38,7 +42,10 @@ CREATE TABLE IF NOT EXISTS user_roles (
 CREATE TABLE public.categorias (
     categoria_id BIGSERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL unique,
-    nemonico VARCHAR(100) NOT NULL UNIQUE
+    nemonico VARCHAR(100) NOT NULL unique,
+    estado boolean default true,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 );
 
 -- 2. Subcategorias (Deportes, Sub-eventos, Tipos de comida)
@@ -51,6 +58,8 @@ CREATE TABLE public.subcategorias (
     fecha_hora TIMESTAMP WITHOUT TIME ZONE,
     proximo BOOLEAN default true, -- CAMPO AÑADIDO: Para indicar si el equipo está activo
     estado BOOLEAN default true, -- CAMPO AÑADIDO: Para indicar si el equipo está activo
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (categoria_id) REFERENCES categorias (categoria_id) ON DELETE CASCADE
 );
 
@@ -58,6 +67,8 @@ CREATE TABLE public.subcategorias (
 CREATE TABLE public.subcategoria_roles (
     subcategoria_id INT NOT NULL,
     rol_id BIGINT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (subcategoria_id, rol_id),
     FOREIGN KEY (subcategoria_id) REFERENCES subcategorias (subcategoria_id) ON DELETE CASCADE,
     FOREIGN KEY (rol_id) REFERENCES roles (id) ON DELETE RESTRICT
@@ -69,6 +80,8 @@ CREATE TABLE public.series (
     subcategoria_id INT NOT NULL,
     nombre_serie VARCHAR(100) NOT NULL,
     estado BOOLEAN default true,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (subcategoria_id, nombre_serie),
     FOREIGN KEY (subcategoria_id) REFERENCES subcategorias (subcategoria_id) ON DELETE CASCADE
 );
@@ -79,7 +92,9 @@ CREATE TABLE public.estadios (
     estadio_id BIGSERIAL PRIMARY KEY, -- Clave primaria para identificar el estadio
     nombre VARCHAR(150) NOT NULL UNIQUE, -- Nombre del estadio
     detalle TEXT, -- Campo para agregar más información/detalle (opcionalmente VARCHAR)
-    estado BOOLEAN default true
+    estado BOOLEAN default true,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 );
 
 -- 3. Equipos (Asociados a un deporte/subcategoría)
@@ -90,6 +105,8 @@ CREATE TABLE public.equipos (
     nombre VARCHAR(100) NOT NULL,
     fundacion DATE,
     estado BOOLEAN default true, -- CAMPO AÑADIDO: Para indicar si el equipo está activo
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (subcategoria_id) REFERENCES subcategorias (subcategoria_id) ON DELETE CASCADE,
     FOREIGN KEY (serie_id) REFERENCES series (serie_id) ON DELETE RESTRICT
 );
@@ -105,7 +122,9 @@ CREATE TABLE public.jugadores (
     apellido VARCHAR(50) NOT NULL,
     fecha_nacimiento DATE,
     estado boolean default true, -- CAMPO AÑADIDO: Para indicar si el jugador está activo
-    documento_identidad VARCHAR(20) UNIQUE
+    documento_identidad VARCHAR(20) unique,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 );
 
 -- 5. Plantilla (Jugadores por equipo)
@@ -114,6 +133,8 @@ CREATE TABLE public.plantilla (
     jugador_id INT NOT NULL,
     numero_camiseta INT,
     rol_id BIGINT NOT NULL, -- CAMPO CORREGIDO: Referencia el ID (BIGINT) de la tabla 'roles', no el 'name'
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (equipo_id, jugador_id),
     FOREIGN KEY (equipo_id) REFERENCES equipos (equipo_id) ON DELETE CASCADE,
     FOREIGN KEY (jugador_id) REFERENCES jugadores (jugador_id) ON DELETE RESTRICT,
@@ -137,8 +158,10 @@ CREATE TABLE public.encuentros (
     
     titulo VARCHAR(150) NOT NULL,
     fecha_hora TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    estado VARCHAR(50) DEFAULT 'Pendiente',
+    estado VARCHAR(50) DEFAULT 'PROGRAMADO',
     activo boolean default true,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 
     -- RELACIÓN 1: SUB-CATEGORÍAS
     FOREIGN KEY (subcategoria_id) REFERENCES public.subcategorias (subcategoria_id) ON DELETE RESTRICT,
@@ -163,6 +186,8 @@ CREATE TABLE public.participacion_encuentro (
     equipo_id INT NOT NULL,
     es_local BOOLEAN DEFAULT FALSE,
     goles_puntos INT DEFAULT 0,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (encuentro_id, equipo_id),
     FOREIGN KEY (encuentro_id) REFERENCES encuentros (encuentro_id) ON DELETE CASCADE,
     FOREIGN KEY (equipo_id) REFERENCES equipos (equipo_id) ON DELETE RESTRICT
@@ -180,6 +205,8 @@ CREATE TABLE public.tabla_posiciones (
     goles_a_favor INT DEFAULT 0,     -- CAMPO AÑADIDO: Goles/puntos anotados
     goles_en_contra INT DEFAULT 0,   -- CAMPO AÑADIDO: Goles/puntos recibidos
     diferencia_goles INT DEFAULT 0,  -- CAMPO AÑADIDO: Diferencia de goles/puntos
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (subcategoria_id, equipo_id),
     FOREIGN KEY (subcategoria_id) REFERENCES subcategorias (subcategoria_id) ON DELETE CASCADE,
     FOREIGN KEY (equipo_id) REFERENCES equipos (equipo_id) ON DELETE CASCADE
@@ -194,6 +221,8 @@ CREATE TABLE public.sanciones (
     motivo TEXT,
     detalle_sancion TEXT,
     fecha_registro DATE DEFAULT CURRENT_DATE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (jugador_id) REFERENCES jugadores (jugador_id) ON DELETE RESTRICT,
     FOREIGN KEY (encuentro_id) REFERENCES encuentros (encuentro_id) ON DELETE SET NULL
 );
