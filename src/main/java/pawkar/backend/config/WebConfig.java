0bf3337422c08b1416cc6bc784c0d6d168792ea5
Carlos Vicente Tontaquimba Quinchuqui <Carlos.Tontaquimba@fj.local>
2025-12-24 11:31:36 -0500

    aceptacion de peticion desde el front

--- src/main/java/pawkar/backend/config/WebConfig.java
+++ src/main/java/pawkar/backend/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://694c1443440e5c00080e9fb4--rococo-pie-a53d5a.netlify.app/") // Reemplaza con tu URL real
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
